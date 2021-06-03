f78eab6e1a5bf634d7f44cb7c0eecb604a4f85be
unknown <seivey@hotmail.com>
2021-06-02 20:25:18 -0600

    mvp done

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Route, Link, useHistory } from 'react-router-dom';
+import Movie from './Movie';
+
 
 export default function MovieList(props) {
   return (
@@ -10,18 +13,27 @@ export default function MovieList(props) {
   );
 }
 
+
+
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+  const { title, director, metascore, id } = props.movie;
+  const history = useHistory()
+  const routeToMovie = () => history.push(`/movies/${id}`)
+  
 
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
+
+      <div className="movie-card" onClick={routeToMovie}>
+        
+          <h2>{title}</h2>
+          <div className="movie-director">
+            Director: <em>{director}</em>
+          </div>
+          <div className="movie-metascore">
+            Metascore: <strong>{metascore}</strong>
+          </div>
+       
       </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-    </div>
+    
   );
 }
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { useHistory } from 'react-router';
+import MovieList from './Movie';
+import {Link } from 'react-router-dom'
 
 export default function SavedList(props) {
+
+  // const returnHome = () => {
+   
+  //   history.push('/')
+  // }
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
       {props.list.map(movie => (
         <span className="saved-movie">{movie.title}</span>
       ))}
-      <div className="home-button">Home</div>
+      <Link to='/'>
+        <div className="home-button" > Home </div>
+      </Link>
     </div>
   );
 }
